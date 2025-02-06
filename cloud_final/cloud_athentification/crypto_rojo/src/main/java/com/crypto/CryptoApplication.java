71c00150e925ad851b1e329e3af389709a5bd9a4
MirindraAndria <mixandria19@gmail.com>
2025-02-06 17:33:48 +0300

    commit cours crypto firebase

--- cloud_final/cloud_athentification/crypto_rojo/src/main/java/com/crypto/CryptoApplication.java
+++ cloud_final/cloud_athentification/crypto_rojo/src/main/java/com/crypto/CryptoApplication.java
@@ -2,9 +2,10 @@ package com.crypto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling 
 public class CryptoApplication {
     public static void main(String[] args) {
         SpringApplication.run(CryptoApplication.class, args);
